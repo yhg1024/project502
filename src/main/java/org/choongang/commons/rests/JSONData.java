a6e93b7f5beaa5a0733ede31a2ca966969209d9a
yhg1024 <dgr1024kr@naver.com>
2023-12-29 15:28:21 +0900

    errorPage setting

---
+++ src/main/java/org/choongang/commons/rests/JSONData.java
@@ -0,0 +1,17 @@
+package org.choongang.commons.rests;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class JSONData<T> {
+    private HttpStatus status = HttpStatus.OK;
+    private boolean sucess = true;
+    private T data;
+    private String message;
+}
